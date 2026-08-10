98584f71d9fddf0852666b6bb813f9ea451994e6
ihoyos57 <ihoyos573@soyudemedellin.edu.co>
2026-08-09 21:39:40 -0500

    🔗 conecta la aplicación con PostgreSQL

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from sqlalchemy import text
+from app.database import engine
 
 app = FastAPI(title="API de Videojuegos")
 
 @app.get("/")
 def root():
     return {"mensaje": "API de Videojuegos funcionando"}
+
+@app.get("/test-db")
+def test_db():
+    with engine.connect() as connection:
+        result = connection.execute(text("SELECT version();"))
+        version = result.fetchone()
+    return {"conexion": "exitosa", "postgres_version": version[0]}
